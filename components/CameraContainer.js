117c4d857850fe24560362cd83fae74170bd5d88
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2017-01-03 20:27:05 -0600

    Small camera changes, mostly modifying native code, though.

--- components/CameraContainer.js
+++ components/CameraContainer.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, TouchableHighlight } from 'react-native';
+
+import Camera from 'react-native-camera';
 
 export default class CameraContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: 'Loading...'
+    };
+  }
+
+  updateData() {
+    this.camera.getZoom()
+      .then((data) => {
+        this.state.text = String(data);
+        this.forceUpdate();
+      })
+  }
+
+  updateFocus() {
+    this.state.text = "FOCUSING";
+    this.forceUpdate();
+    this.camera.focusCenter()
+      .then((data) => {
+        this.state.text = String(data);
+        this.forceUpdate();
+      })
+  }
+
   render() {
     return (
-      <View style={styles.CameraContainer}>
-      </View>
+        <Camera style={styles.CameraContainer}
+        ref={(cam) => {
+          if (!this.camera) {
+            this.camera = cam;
+            this.camera.defaultOnFocusComponent = false;
+          }
+        }}
+        aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.Text}
+          onPress={this.updateData.bind(this)}>
+            {this.state.text}
+          </Text>
+        </Camera>
     );
   }
 }
@@ -17,7 +56,16 @@ var styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    zIndex: -1,
+    zIndex: -10,
     backgroundColor: 'green'
   },
+
+  Text: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 })
